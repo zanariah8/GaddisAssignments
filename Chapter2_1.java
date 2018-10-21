614b570f5950273918cf06f3b12a27807bc7fccb
Alex <alexbae1016@gmail.com>
2018-10-21 16:50:02 -0400

    Uploaded Problem 2-1, from Gaddis Chapter 2
    Gaddis Chapter 2, Problem 1: Name, Age, and Annual Income
    Utilized JOptionPane, and DecimalFormat to store input, and stylize output, respectively

---
+++ Chapter2_1.java
@@ -0,0 +1,40 @@
+package chapter2;
+
+/*
+ * 1.  Name, Age and Annual Income
+ * Write a program that declares the following:
+ * 	- a String variable named name
+ * 	- an int variable named age
+ * 	- a double variable named annualPay
+ * 
+ * 	Story your age, name, and desired annual income as literals in these variables.  
+ * 	The program should display these values on the output
+ */
+
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+public class Chapter2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Utilize the DecimalFormat class to format text
+		DecimalFormat currency = new DecimalFormat("###,##0.00"); 
+		//placeholder variable to store input for later conversion
+		String str; 
+		
+		String name = JOptionPane.showInputDialog("What is your name?");
+		
+		str = JOptionPane.showInputDialog("What is your age?");
+		int age = Integer.parseInt(str);
+		
+		str = JOptionPane.showInputDialog("How much do you make per year?");
+		double annualPay = Double.parseDouble(str); 
+		
+		System.out.println("My name is " + name + ", my age is " + age + ", and I hope "
+				+ "to earn $" + currency.format(annualPay) + " per year");
+		
+		
+	}
+
+}
